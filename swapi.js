df1eb3d60de091e90b0509095d6caaa2e3a6b9ff
corey <23224961+whoiscorey@users.noreply.github.com>
2026-08-29 14:37:59 -0400

    fix: fix arg handling in swapi function and convert if to switch

--- swapi.js
+++ swapi.js
@@ -1,5 +1,3 @@
-const categories = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
-
 function swapiFetch(category) {
   const baseUrl = `https://swapi.info/api`
   fetch(`${baseUrl}/${category}`)
@@ -9,15 +7,19 @@ function swapiFetch(category) {
 }
 
 function swapi() {
+  const categories = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
   const category = process.argv[2]
   const result = categories.find((arg) => arg === `${category}`)
 
-  if (result) {
-    swapiFetch(`${category}`)
-  } else if (!result) {
-    console.log(`available categories: ${categories}`)
-  } else {
-    console.log(`category required: ${categories}`)
+  switch (true) {
+    case !!result:
+      swapiFetch(category)
+      break
+    case !category:
+      console.log(`category required: ${categories}`)
+      break
+    default:
+      console.log(`available categories: ${categories}`)
   }
 }
 
