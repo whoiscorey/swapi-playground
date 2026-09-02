9b5d9d93be49d22e1472c1a535203f943f805784
corey <corey.collins@pdq.com>
2026-09-01 18:12:33 -0600

    convert if to switch, change log to error, move baseUrl and categories variables out of swapiJoe function

--- swapi.js
+++ swapi.js
@@ -1,31 +1,38 @@
+const categories = [
+  "films",
+  "people",
+  "planets",
+  "species",
+  "starships",
+  "vehicles",
+];
+const baseUrl = `https://swapi.info/api`;
+
 function swapiFetch(url) {
   fetch(url)
     .then((res) => res.json())
     .then((json) => console.log(json))
-    .catch((error) => console.error(error))
+    .catch((error) => console.error(error));
 }
 
-function swapi() {
-  const categories = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
+function swapiJoe() {
+  const category = categories.find((arg) => arg === process.argv[2]);
+  const id = process.argv[3];
 
-  const category = categories.find((arg) => arg === process.argv[2])
-  const id = process.argv[3]
-
-  const baseUrl = `https://swapi.info/api`
-  const categoryUrl = `${baseUrl}/${category}`
-  const idUrl = `${categoryUrl}/${id}`
+  const categoryUrl = `${baseUrl}/${category}`;
+  const idUrl = `${categoryUrl}/${id}`;
 
   switch (true) {
     case !!id:
-      swapiFetch(idUrl)
-      break
+      swapiFetch(idUrl);
+      break;
     case !!category:
-      swapiFetch(categoryUrl)
-      break
+      swapiFetch(categoryUrl);
+      break;
     default:
-      console.log('category required:')
-      swapiFetch(baseUrl)
+      console.error("category required:");
+      swapiFetch(baseUrl);
   }
 }
 
-swapi()
+swapiJoe();
